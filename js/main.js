a4d44ef79ce68b1e0dee3b9593cc376f731120bb
Christopher De Camp <cdecamp77@gmail.com>
2017-09-30 14:08:45 -0700

    Basic JS to add markers

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-var currentPLayer;
+function startGame() {
+    document.turn = "X";
+}
+
+function nextMove(square) {
+    square.innerText = document.turn;
+    switchTurn();
+}
+
+function switchTurn() {
+    if (document.turn == "X") {
+        document.turn = "O";
+    } else {
+        document.turn = "X";
+    }
+}
